4be0e28641d0d06bb5ad2a4b2d8cd10bc189837d
Виталий Травин <vtravin@nologostudio.ru>
2023-11-02 12:42:36 +0600

    init project

---
+++ app/reviews/hollow-knight/page.jsx
@@ -0,0 +1,8 @@
+export default function HollowKnightPage() {
+    return (
+        <>
+            <h1>Hollow Knight</h1>
+            <p>This will be review for Hollow Knight.</p>
+        </>
+    );
+}
---
+++ app/reviews/stardew-valley/page.jsx
@@ -0,0 +1,8 @@
+export default function StardewValleyPage() {
+    return (
+        <>
+            <h1>Stardew Valley</h1>
+            <p>This will be review for Stardew Valley .</p>
+        </>
+    );
+}
